0c1163708f6e254b1c54a5721d00b8cb63826c36
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-18 21:56:19 -0300

    Correção no engine do aws-cluster-instance

--- aws-cluster-instance.tf
+++ aws-cluster-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_rds_cluster_instance" "tf_aurora_instance" {
   cluster_identifier  = aws_rds_cluster.tf_aurora_cluster.id
   instance_class      = "db.r6g.large"
-  engine              = aws_rds_cluster.aurora.engine
+  engine              = aws_rds_cluster.tf_aurora_cluster.engine
   apply_immediately   = true
   tags = {
     Name = "aurora-instance"

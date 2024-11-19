7422bd0f776ed4e16cfe45a2b2b1eae25d43fa3f
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-18 22:13:47 -0300

    Correção do cluster_identifier

--- aws-cluster-instance.tf
+++ aws-cluster-instance.tf
@@ -1,5 +1,5 @@
 resource "aws_rds_cluster_instance" "tf_aurora_instance" {
-  cluster_identifier  = aws_rds_cluster.tf_aurora_cluster.id
+  cluster_identifier  = aws_rds_cluster.tf_aurora_cluster.cluster_identifier
   instance_class      = "db.r6g.large"
   engine              = aws_rds_cluster.tf_aurora_cluster.engine
   apply_immediately   = true

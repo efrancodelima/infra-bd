5036353ebcc437ea12d8f1c89ecaeeb4d7de3ba5
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-18 22:26:05 -0300

    Ajuste no terraform

--- aws-aurora-cluster.tf
+++ aws-aurora-cluster.tf
@@ -6,7 +6,9 @@ module "rds_aurora" {
 resource "aws_rds_cluster" "tf_aurora_cluster" {
   cluster_identifier      = "aurora-cluster"
   engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.10.0"
+  engine_version          = "5.7.mysql_aurora.2.03.2"
+  availability_zones      = [var.aws_zone_1, var.aws_zone_2]
+  database_name           = "lanchonete-db"
   master_username         = var.db_username
   master_password         = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.tf_subnet_group.name

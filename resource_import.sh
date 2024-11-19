01b521afafd07f0ad6e98af692d0feeeb8d0927f
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-19 00:25:37 -0300

    Melhoria no log da importação

--- resource_import.sh
+++ resource_import.sh
@@ -55,7 +55,7 @@ get_first_subnet_group_id() {
 CLUSTER_IDENTIFIER="lanchonete-aurora-cluster"
 CLUSTER_EXISTS=$(check_rds_cluster_exists "$CLUSTER_IDENTIFIER")
 if [ "$CLUSTER_EXISTS" == "None" ]; then
-  echo "\nRecurso 'aws_rds_cluster' não encontrado."
+  echo "Recurso 'aws_rds_cluster.tf_aurora_cluster' não encontrado."
 else
   import_resource "aws_rds_cluster" "tf_aurora_cluster" "$CLUSTER_IDENTIFIER"
 fi
@@ -63,7 +63,7 @@ fi
 # Importa a instância do cluster
 AURORA_INSTANCE_ID=$(get_aurora_instance_id "lanchonete-aurora-cluster" "0")
 if [ "$AURORA_INSTANCE_ID" == "None" ]; then
-  echo "\nRecurso 'aws_rds_cluster_instance' não encontrado."
+  echo "Recurso 'aws_rds_cluster_instance.tf_aurora_instance' não encontrado."
 else
   import_resource "aws_rds_cluster_instance" "tf_aurora_instance" "$AURORA_INSTANCE_ID"
 fi
@@ -71,7 +71,7 @@ fi
 # Importa o Security Group
 SECURITY_GROUP_ID=$(get_first_security_group_id "aurora-security-group")
 if [ "$SECURITY_GROUP_ID" == "None" ]; then
-  echo "\nRecurso 'aws_security_group' não encontrado."
+  echo "Recurso 'aws_security_group.tf_aurora_security_group' não encontrado."
 else
   import_resource "aws_security_group" "tf_aurora_security_group" $SECURITY_GROUP_ID
 fi
@@ -79,7 +79,7 @@ fi
 # Importa o Subnet Group
 SUBNET_GROUP_ID=$(get_first_subnet_group_id "aurora-subnet-group")
 if [ "$SUBNET_GROUP_ID" == "" ]; then
-  echo "\nRecurso 'aws_db_subnet_group' não encontrado."
+  echo "Recurso 'aws_db_subnet_group.tf_subnet_group' não encontrado."
 else
   import_resource "aws_db_subnet_group" "tf_subnet_group" "$SUBNET_GROUP_ID"
 fi
@@ -87,7 +87,7 @@ fi
 # Importa a VPC
 VPC_ID=$(get_first_vpc_id "lanchonete-vpc")
 if [ "$SUBNET_GROUP_ID" == "None" ]; then
-  echo "\nRecurso 'aws_vpc' não encontrado."
+  echo "Recurso 'aws_vpc.tf_vpc' não encontrado."
 else
   import_resource "aws_vpc" "tf_vpc" "$VPC_ID"
 fi
@@ -95,28 +95,28 @@ fi
 # Importa as subnets
 PUBLIC_SUBNET_0_ID=$(get_first_subnet_id "lanchonete-public-subnet-0")
 if [ "$PUBLIC_SUBNET_0_ID" == "None" ]; then
-  echo "\nRecurso 'aws_subnet' (tag name = lanchonete-public-subnet-0) não encontrado."
+  echo "Recurso 'aws_subnet.tf_public_subnet[0]' não encontrado."
 else
   import_resource "aws_subnet" "tf_public_subnet[0]" "$PUBLIC_SUBNET_0_ID"
 fi
 
 PUBLIC_SUBNET_1_ID=$(get_first_subnet_id "lanchonete-public-subnet-1")
 if [ "$PUBLIC_SUBNET_1_ID" == "None" ]; then
-  echo "\nRecurso 'aws_subnet' (tag name = lanchonete-public-subnet-1) não encontrado."
+  echo "Recurso 'aws_subnet.tf_public_subnet[1]' não encontrado."
 else
   import_resource "aws_subnet" "tf_public_subnet[1]" "$PUBLIC_SUBNET_1_ID"
 fi
 
 PRIVATE_SUBNET_0_ID=$(get_first_subnet_id "lanchonete-private-subnet-0")
 if [ "$PRIVATE_SUBNET_0_ID" == "None" ]; then
-  echo "\nRecurso 'aws_subnet' (tag name = lanchonete-private-subnet-0) não encontrado."
+  echo "Recurso 'aws_subnet.tf_private_subnet[0]' não encontrado."
 else
   import_resource "aws_subnet" "tf_private_subnet[0]" "$PRIVATE_SUBNET_0_ID"
 fi
 
 PRIVATE_SUBNET_1_ID=$(get_first_subnet_id "lanchonete-private-subnet-1")
 if [ "$PRIVATE_SUBNET_1_ID" == "None" ]; then
-  echo "\nRecurso 'aws_subnet' (tag name = lanchonete-private-subnet-1) não encontrado."
+  echo "Recurso 'aws_subnet.tf_private_subnet[1]' não encontrado."
 else
   import_resource "aws_subnet" "tf_private_subnet[1]" "$PRIVATE_SUBNET_1_ID"
 fi

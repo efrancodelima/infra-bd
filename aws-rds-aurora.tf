71868b0b0f60ebce4519e4a54c8a323743b39502
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-18 23:12:49 -0300

    Ajuste no módulo rds_aurora

--- aws-rds-aurora.tf
+++ aws-rds-aurora.tf
@@ -1,8 +1,4 @@
-module "rds_aurora" {
-  source  = "terraform-aws-modules/rds-aurora/aws"
-  version = "9.10.0"
-
-  name                    = "aurora-cluster"
+resource "aws_rds_cluster" "tf_aurora_cluster" {
   cluster_identifier      = "aurora-cluster"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.03.2"
@@ -12,6 +8,18 @@ module "rds_aurora" {
   master_password         = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.tf_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.tf_aurora_security_group.id]
-  instance_class          = "db.r6g.large"
   apply_immediately       = true
+}
+
+resource "aws_rds_cluster_instance" "tf_aurora_instance" {
+  count               = 2
+  identifier          = "aurora-instance-${count.index}"
+  cluster_identifier  = aws_rds_cluster.tf_aurora_cluster.cluster_identifier
+  instance_class      = "db.r6g.large"
+  engine              = aws_rds_cluster.tf_aurora_cluster.engine
+  engine_version      = aws_rds_cluster.tf_aurora_cluster.engine_version
+  apply_immediately   = true
+  tags = {
+    Name = "aurora-instance-${count.index}"
+  }
 }

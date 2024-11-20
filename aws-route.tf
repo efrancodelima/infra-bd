d38c888923cbb033e26d27030afc6e68eb130167
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-20 18:29:36 -0300

    Correção em aws-route

--- aws-route.tf
+++ aws-route.tf
@@ -3,8 +3,4 @@ resource "aws_route" "tf_route" {
   route_table_id         = aws_route_table.tf_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.tf_internet_gateway.id
-
-  tags = {
-    Name = "lanchonete-route"
-  }
 }

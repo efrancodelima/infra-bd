d95fcd573817c72124c4516160886c366cef892f
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-20 18:47:24 -0300

    Mudanção da instance_class no cluster Aurora

--- aws-aurora-instance.tf
+++ aws-aurora-instance.tf
@@ -2,7 +2,7 @@ resource "aws_rds_cluster_instance" "tf_aurora_instance" {
   count               = 1
   identifier          = "aurora-instance-${count.index}"
   cluster_identifier  = aws_rds_cluster.tf_aurora_cluster.cluster_identifier
-  instance_class      = "db.t3.small"
+  instance_class      = "db.t3.medium"
   engine              = aws_rds_cluster.tf_aurora_cluster.engine
   engine_version      = aws_rds_cluster.tf_aurora_cluster.engine_version
   apply_immediately   = true

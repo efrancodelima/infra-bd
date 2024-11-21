2f74b7e2ad007942482e8355e42754904b7063d0
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-20 21:45:19 -0300

    Mudança nos recursos

--- resource_import.sh
+++ resource_import.sh
@@ -10,7 +10,6 @@ PRIVATE_SUBNET_ID_1="subnet-0b03f410da640c4cb"
 PUBLIC_SUBNET_ID_0="subnet-04210519054c41980"
 PUBLIC_SUBNET_ID_1="subnet-0dcb9a941b96ae94b"
 ROUTE_TABLE_ID="rtb-0d2b24549dac7dc8e"
-ROUTE="0.0.0.0/0"
 SECURITY_GROUP_ID="sg-06ac17d92f40b0cd5"
 SUBNET_GROUP_NAME="aurora-subnet-group"
 VPC_ID="vpc-0ea4cbbd6e92e3abe"
@@ -30,16 +29,16 @@ import_resource() {
   "$resource_type.$resource_name" "$resource_id" || true
 }
 
-# Importa o cluster Aurora
+# Importa o cluster aurora
 import_resource "aws_rds_cluster" "tf_aurora_cluster" "${AURORA_CLUSTER_NAME}"
 
 # Importa a instância do cluster
 import_resource "aws_rds_cluster_instance" "tf_aurora_instance" "${AURORA_INSTANCE_NAME}"
 
-# Importa o DHCP Options
+# Importa o DHCP options
 import_resource "aws_vpc_dhcp_options" "tf_dhcp_options" "${DHCP_OPTIONS_ID}"
 
-# Importa o Internet Gateway
+# Importa o internet gateway
 import_resource "aws_internet_gateway" "tf_internet_gateway" "${INTERNET_GATEWAY_ID}"
 
 # Importa as subnets
@@ -48,17 +47,17 @@ import_resource "aws_subnet" "tf_public_subnet[1]" "${PUBLIC_SUBNET_ID_1}"
 import_resource "aws_subnet" "tf_private_subnet[0]" "${PRIVATE_SUBNET_ID_0}"
 import_resource "aws_subnet" "tf_private_subnet[1]" "${PRIVATE_SUBNET_ID_1}"
 
-# Importa a Route Table
+# Importa a route table
 import_resource "aws_route_table" "tf_route_table" "${ROUTE_TABLE_ID}"
 
-# Importa a Route
-import_resource "aws_route" "tf_route" "${ROUTE_TABLE_ID}_${ROUTE}"
+# Importa as associações entre a route table e as subnets
+terraform import aws_route_table_association.tf_route_table_association_1 ${ROUTE_TABLE_ID}/${PUBLIC_SUBNET_ID_0}
+terraform import aws_route_table_association.tf_route_table_association_1 ${ROUTE_TABLE_ID}/${PUBLIC_SUBNET_ID_1}
 
-
-# Importa o Security Group
+# Importa o security group
 import_resource "aws_security_group" "tf_aurora_security_group" "${SECURITY_GROUP_ID}"
 
-# Importa o Subnet Group
+# Importa o subnet group
 import_resource "aws_db_subnet_group" "tf_subnet_group" "${SUBNET_GROUP_NAME}"
 
 # Importa a VPC

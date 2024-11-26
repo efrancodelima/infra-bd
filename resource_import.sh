2eaa1bb5cdd00e598e969820fd33da6f3255cfe1
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-26 17:38:51 -0300

    Correção do security group id no import

--- resource_import.sh
+++ resource_import.sh
@@ -17,20 +17,16 @@ import_resource() {
 # Constantes
 AURORA_CLUSTER_NAME="lanchonete-aurora-cluster"
 AURORA_CLUSTER_INSTANCE_NAME="lanchonete-aurora-cluster-instance"
-AURORA_DB_INSTANCE_NAME="lanchonete-aurora-cluster-instance-1"
 ROLE_NAME="lanchonete-aurora-role"
 POLICY_NAME="AmazonRDSFullAccess"
-SECURITY_GROUP_ID="sg-029d2fafc37f26157"
+SECURITY_GROUP_ID="sg-0b9ded624a8d10261"
 SUBNET_GROUP_NAME="lanchonete-aurora-subnet-group"
 
 # Importa o cluster aurora
 import_resource "aws_rds_cluster" "tf_aurora_cluster" "${AURORA_CLUSTER_NAME}"
 
-# Importa a instância do cluster
+# Importa o cluster instance
 import_resource "aws_rds_cluster_instance" "tf_aurora_instance" "${AURORA_CLUSTER_INSTANCE_NAME}"
-
-# Importa a instância db
-import_resource "aws_rds_cluster_instance" "tf_aurora_instance" "${AURORA_DB_INSTANCE_NAME}"
 
 # Importa a role usada no cluster aurora
 import_resource "aws_iam_role" "tf_aurora_role" "${ROLE_NAME}"

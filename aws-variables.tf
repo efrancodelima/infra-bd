d067e254b9ebc8018644b49e3b805e2cbe4a1ee5
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-26 22:48:29 -0300

    Ajuste nas secrets

--- aws-variables.tf
+++ aws-variables.tf
@@ -2,16 +2,19 @@
 variable "aws_region" {
   description = "AWS region"
   type = string
+  default = "us-east-1"
 }
 
 variable "aws_zone_1" {
   description = "AWS AZ 1"
   type = string
+  default = "us-east-1a"
 }
 
 variable "aws_zone_2" {
   description = "AWS AZ 2"
   type = string
+  default = "us-east-1b"
 }
 
 # Banco de dados Aurora
